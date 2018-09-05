3e8feee910451da0e9074f5cef85510d9485896c
Lafif Astahdziq <hello@lafif.me>
2018-09-06 03:33:36 +0700

    update sample

--- example.php
+++ example.php
@@ -18,24 +18,24 @@ function sample_wpbp_process( $process ){
 	);
 
 	$process['foo-posts'] = array(
-		'name' => 'Convert Workshop Date to correct format',
+		'name' => 'Sample bulk process for posts',
 		'type' => 'post',
 		'args' => array(
 			'posts_per_page' => 10,
 			'post_type' => 'post',
 		),
-		'callback' => 'convert_workshop_date',
+		'callback' => 'foo_add_post_meta',
 	);
 
 	return $process;
 }
 
-function foo_add_user_metas($user){
+function foo_add_user_metas( $user ){
 	update_user_meta( $user->ID, 'test_bulk_process', current_time( 'mysql' ) );
 	wpbp_add_success_message( sprintf( __('Added new user meta `test_bulk_process` for user %s (%d)', 'wpbp'), $user->display_name, $user->ID ) );
 }
 
-function convert_workshop_date( $post ) {
+function foo_add_post_meta( $post ) {
 
 	update_post_meta( $post->ID, 'test_bulk_process', current_time( 'mysql' ) );
 	wpbp_add_success_message( sprintf( __('Added new post meta `test_bulk_process` for post %s (%d)', 'wpbp'), $post->post_title, $post->ID ) );
